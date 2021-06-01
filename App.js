880d368c4bf7b9c3c1b301659be0d7c6a6095369
khalilhamdii <khaalil.hamdi@gmail.com>
2021-06-01 14:50:24 +0100

    Add card component + styling

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { View } from 'react-native';
+
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import Card from './app/components/Card';
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{ backgroundColor: 'lightgray', padding: 20, paddingTop: 100 }}
+    >
+      <Card
+        title=" Red Jacket for sale"
+        subTitle="$100"
+        image={require('./app/assets/jacket.jpg')}
+      />
+    </View>
+  );
 }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Text, Platform } from 'react-native';
+import { Text, Platform, StyleSheet } from 'react-native';
 
-const AppText = ({ children }) => {
-  return <Text style={styles.text}>{children}</Text>;
+const AppText = ({ children, style }) => {
+  return <Text style={[styles.text, style]}>{children}</Text>;
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 18,
-    fontFamily: Platform.OS === 'android ? ',
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
   },
 });
 
